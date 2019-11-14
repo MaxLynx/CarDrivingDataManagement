0082de809d19c57e1fbe02b002ea012a30ecd18b
Maksym Zhyrov <zhirmax@ukr.net>
2019-11-14 21:34:25 +0100

    entity classes created

---
+++ Entity/Driver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDrivingDataManagement.Entity
+{
+    public class Driver
+    {
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public Int64 CardID { get; set; }
+        public DateTime CardEndDate { get; set; }
+        public Boolean DrivingForbidden { get; set; }
+
+        public Int32 RuleViolationsCount { get; set; }
+
+        public Driver() { }
+
+        public Driver(String name, String surname, int cardID, DateTime cardEndTime, bool drivingForbidden, int ruleViolationsCount)
+        {
+            Name = name;
+            Surname = surname;
+            CardID = cardID;
+            CardEndDate = cardEndTime;
+            DrivingForbidden = drivingForbidden;
+            RuleViolationsCount = ruleViolationsCount;
+        }
+    }
+}
---
+++ Entity/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDrivingDataManagement.Entity
+{
+    public class Vehicle
+    {
+        public String ID { get; set; }
+        public String VIN { get; set; }
+        public Int32 RepairingsCount { get; set; }
+        public Int32 DriveWeight { get; set; }
+        public Boolean Searched { get; set; }
+        public DateTime STKEndDate { get; set; }
+        public DateTime EKEndDate { get; set; }
+
+        public Vehicle() { }
+
+        public Vehicle(String id, String vin, int repairingsCount, int driveWeight, bool searched,
+            DateTime stkEndDate, DateTime ekEndDate)
+        {
+            ID = id;
+            VIN = vin;
+            RepairingsCount = repairingsCount;
+            DriveWeight = driveWeight;
+            Searched = searched;
+            STKEndDate = stkEndDate;
+            EKEndDate = ekEndDate;
+        }
+    }
+}
