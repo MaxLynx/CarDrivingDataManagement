8e0d0330feb0c8e3335ea331b5dc08d0fa1193bb
Maksym Zhyrov <zhirmax@ukr.net>
2019-11-14 21:43:34 +0100

    Record and Block utils classes created

---
+++ Utils/Block.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDrivingDataManagement.Utils
+{
+    public class Block
+    {
+        public Int32 Size { get; set; }
+        public byte[] ByteArray { get; set; }
+        public List<Record> Records { get; set; }
+    }
+}
---
+++ Utils/Record.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDrivingDataManagement.Utils
+{
+    public class Record
+    {
+        public Int32 Size { get; set; }
+        public Int32 TrueSize { get; set; }
+        public byte[] ByteArray { get; set; }
+        public Boolean Used { get; set; }
+    }
+}
